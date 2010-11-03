69c2dfac7a41b5e70852e77122a5936b55740144
liquid <liquid@apollo.localdomain>
2010-11-03 07:48:15 -0500

    Pushing modified registration system

--- mods/employers.php
+++ mods/employers.php
@@ -1,6 +1,7 @@
 <?php
-
+echo "<div class='tempcolor'>";
 $regrep    = $_GET['rep'];
+$newcmp	   = $_GET['ncp'];
 $signed_in = $_SESSION['LOGGED_IN'];
 //if the user session is not set have them try to login or create a profile
 if ($regrep == 1)
@@ -15,5 +16,64 @@ else
 	include("$serverroot/mods/signin.php");
 }
 //if it is set provide them a panel to change their company info
+//
+if($signed_in == '1')
+{
+	$Email = $_SESSION['Email'];
+
+	if(($_SESSION['CID'] == '0') && ($newcmp != 1))
+	{
+		Tools::InitMysql();
+		$company = substr(strstr($Email, '@'), 1);
+		$explode_company = explode(".", $company);
+		$company = $explode_company[count($explode_company)-2];
+		echo "<p>Now to register you with a company</p>";
+		echo "<p>We have the following companies listed based on your email address:<strong> $company</strong><br /> <br /> If this is correct please select the company and hit add company or <a href='/index.php?disp=e&ncp=1'>Register a New One</a></p>";
+
+		$search_name= "%$company%";
+		$auto_query = "Select * From Company WHERE Company_name LIKE '$search_name' OR URL LIKE '$search_name'";
+		$find_company = Tools::Query($auto_query);
+		$company_count = 0;
+
+		echo "<form action = 'index.php?disp=e' method='post' enctype='multipart/form-data'>";
+		while($row = mysql_fetch_array($find_company))
+		{
+			$CID = $row['CID'];
+			$Company_name = $row['Company_name'];
+			$URL = $row['URL'];
+			$Address= $row['Address'];
+			echo "<p><input type='radio' name='found_companies' value='$CID'>";
+			echo "  <strong>$Company_name</strong>";
+			echo "  | $URL";
+			echo "  | $Address</p>";
+
+			$company_count++;
+		}
+
+		if ($company_count != 0)
+		{
+
+			echo "<input type = 'submit' value = 'Add Company' name 'Add Company to profile' />";
+		}else
+		{
+			echo "It looks like $company was not found, please register here";
+		}
+		echo "</form>";
+
+
+
+		//Look up possible matches for that company based on their email address domain
+		//tokeninze the string possibly
+		//present them the option to choose from a list of prefilled
+		//if company profile doesn't exist then ask them to create one
+
+	}
+	if(($_SESSION['CID'] == '0') && ($newcmp == 1))
+	{
+		include("$serverroot/mods/employer_register.php");
+	}
+
+}
+echo "</div>";
 
 ?>
--- mods/signin.php
+++ mods/signin.php
@@ -1,19 +1,19 @@
 <?php
 require_once("$serverroot/lib/tools/libload.php");
 
-$user_sub = $_POST['user']; //Submitted Username
+$User_Post= $_POST['user']; //Submitted Username
 $pwd_sub  = $_POST['pwd'];  //Submitted Password
 
 //Perform the query only if those values have something in them
-if ((isset($user_sub)) && (isset($pwd_sub)) && !isset($_SESSION['LOGGED_IN']))
+if ((isset($User_Post)) && (isset($pwd_sub)) && !isset($_SESSION['LOGGED_IN']))
 {
     Tools::InitMysql();
     //We distinguist between company and committies based on if they entered
     //an email address
-    if(strpbrk($user_sub, '@') == FALSE)
+    if(strpbrk($User_Post, '@') == FALSE)
     {
         //this conditional basically means we on the student committee signing in
-        $user_query= "Select * FROM expo_committee where netid ='$user_sub'";
+        $user_query= "Select * FROM expo_committee where netid ='$User_Post'";
         $query = Tools::Query($user_query);
         while($row = mysql_fetch_array($query))
         {
@@ -43,12 +43,12 @@ if ((isset($user_sub)) && (isset($pwd_sub)) && !isset($_SESSION['LOGGED_IN']))
     }else
             {
                 //This loop basically means we are a corporate representative signing in
-                $user_query= "Select * FROM Representatives where email ='$user_sub'";
+                $user_query= "Select * FROM Representatives where Email ='$User_Post'";
                 $query = Tools::Query($user_query);
                 while($row = mysql_fetch_array($query))
                 {
                     //so if the user kind of fails to login lets boot him mang
-                    if($row['password']!= md5($pwd_sub))
+                    if($row['Password']!= md5($pwd_sub))
                     { 
                         //should probably pretty print this or something
                         //Div class that outputs error messages
@@ -56,16 +56,14 @@ if ((isset($user_sub)) && (isset($pwd_sub)) && !isset($_SESSION['LOGGED_IN']))
                         break;
                     }else
                     {
-                        $_SESSION['netid']      =  $row['netid'];
-                        $_SESSION['first_name'] =  $row['first_name'];
-                        $_SESSION['last_name']  =  $row['last_name'];
-                        $_SESSION['pref_email'] =  $row['pref_email'];
-                        $_SESSION['year']       =  $row['year'];
-                        $_SESSION['print_name'] =  $row['print_name'];
-                        $_SESSION['permissions']=  $row['permissions'];
-                        $_SESSION['position']   =  $row['position'];
-                        $_SESSION['active']     =  $row['active'];
-                        $_SESSION['shirt_size'] =  $row['shirt_size'];
+                        $_SESSION['Rep_ID']     =  $row['Rep_ID'];
+                        $_SESSION['First_Name'] =  $row['First_Name'];
+                        $_SESSION['Last_Name']  =  $row['Last_Name'];
+                        $_SESSION['Tel']        =  $row['Tel'];
+                        $_SESSION['Email']      =  $row['Email'];
+                        $_SESSION['CID']        =  $row['CID'];
+                        $_SESSION['Fax']        =  $row['Fax'];
+                        $_SESSION['Actived']     = $row['Actived'];
                         $_SESSION['LOGGED_IN'] = '1';
                     }
 
